582105cd7d0c7cda203f193e67e5080298a149a9
Dylan <dylanelias201931369@cunoc.edu.gt>
2025-08-22 01:23:46 -0600

    chore: add base java project

---
+++ java/src/test/java/edu/ss1/bpmn/BuenoParaMasNadaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ss1.bpmn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuenoParaMasNadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
